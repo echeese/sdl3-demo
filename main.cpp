a926171296f2a93822aa973f056f19babd6b4ed7
echeese <echeese@users.noreply.github.com>
2024-10-05 12:30:16 -0400

    Add shell, improve Windows support

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-static SDL_Window *window = NULL;
-static SDL_Renderer *renderer = NULL;
+namespace {
+	SDL_Window *window = nullptr;
+	SDL_Renderer *renderer = nullptr;
+}
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -13,7 +15,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("My SDL App", 800, 600, 0, &window, &renderer))
+    if (!SDL_CreateWindowAndRenderer("My SDL App", 800, 600, SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_RESIZABLE, &window, &renderer))
     {
         SDL_Log("Failed to create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -38,9 +40,18 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     {
         case SDL_EVENT_QUIT:
             return SDL_APP_SUCCESS;
+		case SDL_EVENT_KEY_DOWN:
+	    {
+		    if(event->key.key == SDLK_RETURN && event->key.mod & SDL_KMOD_ALT)
+		    {
+			    SDL_SetWindowFullscreen(window, !(SDL_GetWindowFlags(window) & SDL_WINDOW_FULLSCREEN));
+		    }
+            break;
+	    }
         default:
-            return SDL_APP_CONTINUE;
+            break;
     }
+    return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
